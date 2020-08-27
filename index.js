b255fadc932656c147303f04dfde828b11f2ce82
Lescuclai <lea-escudie@hotmail.fr>
2020-08-27 20:59:04 +0200

    cors ok

--- index.js
+++ index.js
@@ -12,16 +12,22 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use((request, response, next) => {
+const corsOptions={
+  origin:'http://localhost:3000',
+  optionsSuccesStatus:200,
+  credentials:true
+}
+app.use(cors(corsOptions));
+
+app.options('*',cors());
+
+/*app.use((request, response, next) => {
   response.header('Access-Control-Allow-Origin', '*');
   response.header('Access-Control-Allow-Credentials', true);
   response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
-});
-
-app.use(cors());
-
+});*/
 
 
 app.use(express.static(__dirname + '/public'));
